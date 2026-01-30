c6797bc6eeb56a09b1cf90cdfd505d4e618bd3a0
kiska73 <55405548+kiska73@users.noreply.github.com>
2026-01-30 19:03:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
+# Avvia il worker (log immediati con -u)
+python -u worker.py &
 
-# 1. Avvia il worker in background (la & alla fine è fondamentale)
-# Questo processo controlla i prezzi e manda le notifiche
-python worker.py &
-
-# 2. Avvia il server web con Gunicorn (più stabile di python web.py)
-# Questo processo risponde alle richieste dell'app e ascolta sulla porta
+# Avvia il server web
 gunicorn web:app --bind 0.0.0.0:$PORT
